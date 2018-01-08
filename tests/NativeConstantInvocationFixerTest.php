90eea3ed7297638f8b14b6789b05f854cd2ac512
Filippo Tessarotto <zoeslam@gmail.com>
2018-01-08 16:36:43 +0100

    Whops

--- tests/NativeConstantInvocationFixerTest.php
+++ tests/NativeConstantInvocationFixerTest.php
@@ -21,7 +21,7 @@ final class NativeConstantInvocationFixerTest extends AbstractFixerTestCase
         $key = 'foo';
 
         $this->expectException(\PhpCsFixer\ConfigurationException\InvalidConfigurationException::class);
-        $this->expectExceptionMessage(\sprintf('[native_constant_invocation] Invalid configuration: The option "%s" does not exist.', $key));
+        $this->expectExceptionMessage(\sprintf('Invalid configuration: The option "%s" does not exist.', $key));
 
         $this->fixer->configure([
             $key => 'bar',
